9a248effbf3d58c6420fbf6b4c898a2db86afaa7
Sami Koskivaara <fisamkosk3@fihava.local>
2019-12-05 14:04:34 +0200

    Simplify axios

--- webrequest.js
+++ webrequest.js
@@ -3,23 +3,14 @@ const axios = require('axios');
 async function webrequest(url, method, payload, headers, username, password) {
   const auth = username && password ? { username, password } : null;
   const config = {
+    url,
+    method,
     auth,
+    data: payload,
     headers
   };
   try {
-    let response = null;
-    if (method === 'post') {
-      response = await axios.post(url, payload, config);
-    } else if (method === 'put') {
-      response = await axios.put(url, payload, config);
-    } else if (method === 'patch') {
-      response = await axios.patch(url, payload, config);
-    } else if (method === 'delete') {
-      response = await axios.delete(url, config);
-    } else {
-      response = await axios.get(url, config);
-    }
-
+    const response = await axios(config);
     return response;
   } catch (error) {
     console.error(error);
